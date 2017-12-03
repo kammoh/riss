2d139843cdfeebe720cc43371a7c65a906e50a20
Kamyar Mohajerani <kammoh@gmail.com>
2017-12-03 16:47:18 -0500

    Import from https://baldur.iti.kit.edu/sat-competition-2017/solvers/main/Riss7.zip

---
+++ coprocessor/version.cc
@@ -0,0 +1,11 @@
+/*
+ * This file was created automatically. Do not change it.
+ * If you want to distribute the source code without
+ * git, make sure you include this file in your bundle.
+ */
+#include "coprocessor/version.h"
+
+const char* Coprocessor::gitSHA1            = "cf1a66e";
+const char* Coprocessor::gitDate            = "Sun May 28 20:45:58 2017";
+const char* Coprocessor::coprocessorVersion = "7.0.0";
+const char* Coprocessor::signature          = "coprocessor 7.0.0 build cf1a66e";
---
+++ riss/utils/version.cc
@@ -0,0 +1,11 @@
+/*
+ * This file was created automatically. Do not change it.
+ * If you want to distribute the source code without
+ * git, make sure you include this file in your bundle.
+ */
+#include "riss/utils/version.h"
+
+const char* Riss::gitSHA1         = "cf1a66e";
+const char* Riss::gitDate         = "Sun May 28 20:45:58 2017";
+const char* Riss::solverVersion   = "7.0.0";
+const char* Riss::signature       = "riss 7.0.0 build cf1a66e";
